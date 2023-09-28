981bccce8a6284c39169bf11344f7c8d575b1866
Levin-ops <artmwanganyi09@outlook.com>
2023-09-29 01:32:16 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+  return (bestCustomer = `not bob`);
+}
+
+function overwriteBestCustomer(){
+    return (bestCustomer=`maybe bob`);
+}
+
+const leastFavoriteCustomer=`Bob`
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer=`Lets see`
+}
